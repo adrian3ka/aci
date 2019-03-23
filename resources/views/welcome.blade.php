8a4105ccc4f9a590a0f1d83e9a11f1dcf82621e8
adrian <eekkaaadrian@gmail.com>
2019-03-24 00:06:06 +0700

    add slick

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,58 @@
 
 @section('css')
     <link href="{{  asset('css/welcome.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{  asset('css/slick.css') }}" rel="stylesheet" type="text/css">
+    
+    <link href="{{  asset('css/slick-theme.css') }}" rel="stylesheet" type="text/css">
+    <style>
+        .main-banner {
+		    border: 2px solid;
+		    border-radius: 40px;
+            overflow: hidden;
+            margin-bottom: 10px;
+		}
+		.slick-prev:before, .slick-next:before { 
+            color:black !important;
+        }
+		.slick-next {
+            height: 100px;
+            display: inline;
+            right: -40px;
+            top: 50%;
+            width: 100px;
+		}
+		.slick-prev {
+            height: 100px;
+            display: inline;
+            left: -40px;
+            top: 50%;
+            width: 100px;
+            z-index: 10;	
+		}
+    </style>
+@endsection
+
+@section('script')
+    <script type="text/javascript" src="{{ asset('js/slick.js') }}" ></script>
+    <script>
+        $(document).ready(function(){
+            $('.main-banner').slick({
+                autoplay: true,
+                autoplaySpeed: 2000,
+                fade: true,
+                arrows: true
+            });
+        });
+    </script>
 @endsection
 
 @section('content')
+
+<div class="main-banner">
+@for ($i = 1; $i <= 3; $i++)
+		<img src="{{ url('img/banner/banner_' .$i .'.jpg') }}">
+@endfor
+</div>
 <p class="subtitle">Merasa tertekan, kesepian, menginginkan tantangan dan ingin menjadi 
 warga senior yang SEHAT, CERIA dan BAHAGIA? Bergabunglah dengan H2C ACI, 
 kita akan merasakan perbedaannya <b>"MUDA DALAM HATI"</b></p>
